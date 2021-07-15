703a8db48b048cc5bc52feacbdbb82fb3ca4bebb
Fineas Berindei <fberindei@dexwell.com>
2021-07-15 22:27:55 +0300

    miau

--- src/app/store/selector.ts
+++ src/app/store/selector.ts
@@ -3,6 +3,14 @@ import { ContactsState } from "../models/contacts.model";
 
 const getContactsState = createFeatureSelector<ContactsState>('contacts');
 
+
 export const getContacts = createSelector(getContactsState, (state) => {
     return state.contacts;
 });
+
+export const getContactById = createSelector(getContactsState, (state, props) => {
+    return state.contacts[props.id] ? state.contacts[props.id] : null;
+
+    
+});
+
